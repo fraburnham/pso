5deefc7558742bf4954c95594d5ca63aeb00dc77
Frank Burnham <frank.e.burnham@gmail.com>
2015-01-05 09:56:04 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject pso "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Particle Swarm Optimization"
+  :url "http://github.com/fraburnham/pso"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
